ce243f5bae7d62c61ebcc71b6902705576b778bc
appalse <frontend-hh@yandex.ru>
2020-03-28 15:57:15 +0300

    use classnames library

--- client/src/Header/Header.js
+++ client/src/Header/Header.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import classnames from 'classnames';
 
 export default function Header({start, settings, history, details}) {
-    let containerHeader = "Container";
-    if (start) containerHeader += " Container_align_header";
-    if (history || details) containerHeader += " Container_align_headerBuild";
-    let buttonIconType = "Button-Icon";
+    let containerClass = classnames({
+        'Container': true,
+        'Container_align_header': start,
+        'Container_align_headerBuild': history || details
+    });
+    let buttonIconClass = classnames({
+        'Button-Icon': true,
+        'Icon_type_build': history,
+        'Icon_type_rebuild': details
+    });
     let buttonText = undefined;
-    if (history) {
-        buttonIconType += " Icon_type_build";
-        buttonText = "Run build";
-    }
-    if (details) {
-        buttonIconType += " Icon_type_rebuild";
-        buttonText = "Rebuild";
-    }
+    if (history) buttonText = "Run build";
+    if (details) buttonText = "Rebuild";
     return (
         <header className="Header">
-            <div className={containerHeader}>
+            <div className={containerClass}>
                 { (start || settings) &&
                     <h1 className="Header-Caption Header-Caption_color_title font_size_xxl Header_spaces_h1">School CI server</h1>
                 }
@@ -26,7 +27,7 @@ export default function Header({start, settings, history, details}) {
                 
                 { start &&
                     <button className="Button Button_color_secondary Button_size_s font_size_s">
-                        <i className={buttonIconType}></i>
+                        <i className={buttonIconClass}></i>
                         <div className="Button-Caption">Settings</div>
                     </button>
                 }
