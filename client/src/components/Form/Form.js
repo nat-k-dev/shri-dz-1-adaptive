a9d16ad68c3a4d989406c5a0c56e0b554b91e79d
appalse <frontend-hh@yandex.ru>
2020-04-02 02:42:37 +0300

    add Input component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Input from './../Input/Input';
 import './Form.scss';
 
-export default function Form() {
+// функция вызова бэкенда, чтобы переслать settings на сервер
+async function callBackendAPI(state) {
+    const response = await fetch('/api/settings', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            repoName: state.repoName,
+            buildCommand: state.buildCommand,
+            mainBranch: state.mainBranch,
+            period: state.period
+        })
+    });
+    if (response.status !== 200) {
+        throw Error(response);
+    }
+};
+
+const initialState = {
+    repoName: '',
+    buildCommand: 'npm build',
+    mainBranch: 'master',
+    period: '10'
+}
+
+export default function Form({history}) {
+    const [state, setState] = useState(initialState);
+    const [disableButton, setDisableButton] = useState(false);
+    function handleSubmitClick(event) {
+        event.preventDefault();
+        callBackendAPI(state)
+            .then(res => {
+                console.log('server response: ', res);
+            })
+            .catch(err => {
+                console.log('catch in callBackendAPI: ', err);
+            })
+            .finally(() => {
+                console.log('Enable buttons');
+                setDisableButton(false);
+            });
+        console.log('Disable buttons');
+        setDisableButton(true);
+    }
+    function handleInputChange(fieldId, fieldValue) {
+        setState({...state, [fieldId]: fieldValue});
+    }
     return (
-        <form className="Form" action="build_history.html" method="post">
+        <form className="Form" onSubmit={handleSubmitClick} method="post">
             <div className="Form-Item_align_vertical Form-Item_verticalSpace_l">
                 <h2 className="Form-Title">Settings</h2>
                 <div className="Form-Description">Configure repository connection and synshronization settings.</div>
@@ -11,37 +59,28 @@ export default function Form() {
 
             <div className="Form-Item_align_vertical Form-Item_spaceBottom_m">
                 <label className="Form-Label Form-Item_spaceBottom_xs">GitHub repository<span className="Form-Label_color_mandatory">*</span></label>
-                <div className="Input">
-                    <input className="Form-Input" type="text" placeholder="user-name/repo-name" required />
-                    <i className="Input-DeleteTextIcon Icon_type_deleteText"></i>
-                </div>
+                <Input id='repoName' placeholder={'user-name/repo-name'} isRequired={true} hasDeleteIcon={true} onChange={handleInputChange} />
             </div>
 
             <div className="Form-Item_align_vertical Form-Item_spaceBottom_m">
                 <label className="Form-Label Form-Item_spaceBottom_xs">Build command<span className="Form-Label_color_mandatory">*</span></label>
-                <div className="Input">
-                    <input className="Form-Input" type="text" placeholder="npm ci && npm run build" required />
-                    <i className="Input-DeleteTextIcon Icon_type_deleteText"></i>
-                </div>
+                <Input id='buildCommand' placeholder={'npm ci && npm run build'} isRequired={true} hasDeleteIcon={true} onChange={handleInputChange} />
             </div>
             
             <div className="Form-Item_align_vertical Form-Item_spaceBottom_l">
                 <label className="Form-Label Form-Item_spaceBottom_xs">Main branch</label>
-                <div className="Input">
-                    <input className="Form-Input" type="text" placeholder="master" />
-                    <i className="Input-DeleteTextIcon Icon_type_deleteText"></i>
-                </div>
+                <Input id='mainBranch' placeholder={'master'} isRequired={false} hasDeleteIcon={true} onChange={handleInputChange} />
             </div>
 
             <div className="Form-Item_align_horizontal Form-Item_spaceBottom_xl">
                 <label className="Form-Label Form-Item_spaceRight_s">Synchronize every </label>
-                <input className="Form-Input Form-InputNumber Form-Input_size_small Form-Item_spaceRight_s" type="text" placeholder="10" />
+                <Input id='period' placeholder={'10'} isRequired={false} hasDeleteIcon={false} onChange={handleInputChange} additionalClasses='Form-InputNumber Form-Input_size_small Form-Item_spaceRight_s' />
                 <label className="Form-Label">minutes</label>
             </div>
 
             <div className="Form-Item_align_adaptive">
-                <button type="submit" className="Button Button_color_primary Button_size_m Form-Button_gap_s font_size_s">Save</button>
-                <button type="button" formAction="start_screen.html" className="Button Button_color_secondary Button_size_m font_size_s">Cancel</button>
+                <button type="submit" disabled={disableButton} className="Button Button_color_primary Button_size_m Form-Button_gap_s font_size_s">Save</button>
+                <button type="button" disabled={disableButton} className="Button Button_color_secondary Button_size_m font_size_s">Cancel</button>
             </div>
 
         </form>
---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './Input.scss';
+
+export default function Input({id, placeholder, isRequired, hasDeleteIcon, onChange, additionalClasses}) {
+
+    const inputClass = additionalClasses ? ('Form-Input ' + additionalClasses) : 'Form-Input';
+
+    const [state, setState] = useState('');
+    function handleChange(event) {
+        setState(event.target.value);
+        onChange(id, event.target.value);
+    };
+    return (
+        <div className="Input">
+            { isRequired ? 
+                (<input className={inputClass} type="text" value={state} onChange={handleChange} placeholder={placeholder} required />) :
+                (<input className={inputClass} type="text" value={state} onChange={handleChange} placeholder={placeholder}  />)
+            }
+            { hasDeleteIcon && 
+                <i className="Input-DeleteTextIcon Icon_type_deleteText"></i>
+            }
+        </div>
+    );
+}
