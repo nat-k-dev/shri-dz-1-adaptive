0b28a5d5ec4a31d8e215e15a4c6fd66ca7f08dad
appalse <frontend-hh@yandex.ru>
2020-04-03 13:04:34 +0300

    add date time formatting

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,7 +1,23 @@
+const monthMap = {
+    0: 'янв',
+    1: 'фев',
+    2: 'мар',
+    3: 'апр',
+    4: 'мая',
+    5: 'июн',
+    6: 'июл',
+    7: 'авг',
+    8: 'сен',
+    9: 'окт',
+    10: 'ноя',
+    11: 'дек'
+}
+
 function convertDateTime(str) {
+    const d = new Date(str);
     return {
-        time: '-',
-        date: '-'
+        time: d.getHours() + ':' + d.getMinutes(),
+        date: d.getDate() + ' ' + monthMap[d.getMonth()]
     }
 }
 
