c02cd5344dd0c9d6c0640475ef4478922ee9b1f6
appalse <frontend-hh@yandex.ru>
2020-04-07 15:09:32 +0300

    add AppRoutes constants

---
+++ client/src/constants/AppRoutes.js
@@ -0,0 +1,9 @@
+const AppRoutes = {
+    HOME: '/',
+    SETTINGS: '/settings',
+    BUILD_ID: '/build/:id',
+    START: '/start',
+    BUILD_HISTORY: '/build_history'
+}
+
+module.exports = { AppRoutes }
