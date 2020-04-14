950c4b621bd9e479b98862212de9a72ae8807aca
appalse <frontend-hh@yandex.ru>
2020-04-14 10:02:54 +0300

    fix test on settings form

--- tests/integration/settings.hermione.js
+++ tests/integration/settings.hermione.js
@@ -11,15 +11,25 @@ describe('Настройки', () => {
             })
     })
     it('обновляются после сохранения', function() {
+        const repoName = 'https://github.com/appalse/sportmaster-task.git';
         return this.browser
             .url('/settings')
             .waitForVisible('#repoName')
             .click('#repoName')
-            .keys('https://github.com/appalse/sportmaster-task.git')
+            .keys(repoName)
             .click('#buildCommand')
             .keys('npm run build')
             .click('#mainBranch')
             .keys('master')
             .submitForm('.Form')
+            .waitForEnabled('.Button')
+            .url('/')
+            .url('/settings')
+            .waitForVisible('#repoName')
+            .getText('#repoName')
+            .then((value) => {
+                console.log('VALUE = ', value);
+                assert.strictEqual(value, repoName, 'Название репозитория не обновилось');
+            })
     })
 });
