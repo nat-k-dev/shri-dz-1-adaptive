f59ee670a84401cdb02a316acbd135279bf3eac9
appalse <frontend-hh@yandex.ru>
2020-05-23 19:34:54 +0300

    translate Footer component

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import './Footer.scss';
 import NavLinks from './../NavLinks/NavLinks';
 import './../Container/Container.scss';
+import { useTranslation } from 'react-i18next';
 
 export default function Footer() {
+    const { t } = useTranslation();
     return (
         <footer className="Footer font_size_s Footer_spaceTop_s">
             <div className="Container Container_align_footer">
                 <NavLinks />
-                <div className="Footer-Copyright">&copy; 2020 Natalia Karaseva</div>
+                <div className="Footer-Copyright">&copy; 2020 {t('website-author')}</div>
             </div>
         </footer>
     );
