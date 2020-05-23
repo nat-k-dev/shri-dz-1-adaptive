ea8f177b84d7d6d7de2969dd22d5224593623e28
appalse <frontend-hh@yandex.ru>
2020-05-23 23:20:21 +0300

    add language detection and remove unnecessary backend

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -1,6 +1,5 @@
-import i18n from "i18next";
-import Backend from 'i18next-locize-backend';
-import detector from "i18next-browser-languagedetector";
+import i18next from "i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from "react-i18next";
 
 import translationEN from './App.keys.EN.json';
@@ -17,13 +16,38 @@ const resources = {
   }
 };
 
-i18n
-  .use(detector)
+
+const LanguageDetectionOptions = {
+    // Порядок и список того, откуда приложение узнает о пользовательском языке
+    order: ['navigator', 'cookie', 'localStorage'],
+   
+    // параметры и ключи для поиска языка из вышеуказанных мест
+    lookupCookie: 'i18next',
+    lookupLocalStorage: 'i18nextLng',
+   
+    // закешировать пользовательский язык тут:
+    caches: ['localStorage', 'cookie'],
+    excludeCacheFor: ['cimode'], 
+   
+    // optional expire and domain for set cookie
+    cookieMinutes: 10,
+    cookieDomain: 'myDomain',
+   
+    // fallback to a similar whitelist language
+    // Example 1: Browser language is 'es'
+    // if 'es' is not found in whitelist, first fallback to any whitelist language that starts with 'es-', then fallback to fallbackLng ('es' -> 'es-*' -> fallbackLng)
+    // Example 2: Browser language is 'es-MX'
+    // if 'es-MX' is not found in whitelist, first fallback to 'es', then fallback to 'es-*', then fallback to fallbackLng ('es-MX' -> 'es' -> 'es-*' -> fallbackLng)
+    checkForSimilarInWhitelist: false,
+   
+  }
+
+i18next
   .use(initReactI18next) // passes i18n down to react-i18next reactI18nextModule initReactI18next
-  .use(Backend)
+  .use(LanguageDetector)  
   .init({
     resources,
-    lng: "en",
+    lng: navigator.language || navigator.userLanguage,
     fallbackLng: "en",  // use en if detected lng is not available  
 
     keySeparator: false, // we do not use keys in form messages.welcome
@@ -31,11 +55,10 @@ i18n
     interpolation: {
       escapeValue: false // react already safes from xss
     },
-    backend: {
-        projectId: '50beb02a-4432-461b-b0f4-0ffbde75caac',
-        apiKey: 'c4543700-7773-4dc2-af5b-fc0aad909812',
-        referenceLng: 'en'
-      }
+    detection: LanguageDetectionOptions
+  }, (err, t) => {
+    if (err) return console.log('something went wrong loading', err);
+    t('key'); // -> same as i18next.t
   });
 
-  export default i18n;
+  export default i18next;
